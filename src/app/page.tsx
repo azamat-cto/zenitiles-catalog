f13e2f16c64b5b86e3fe979265e527edf5cb54cb
azamatcto <azamatkhabibullaevcto@gmail.com>
2024-06-28 21:32:55 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,450 @@
+import Image, { StaticImageData } from "next/image";
+import Image01 from "@/assets/images/img-01.jpg";
+import Image02 from "@/assets/images/img-02.jpg";
+import Image03 from "@/assets/images/img-03.jpg";
+import Image04 from "@/assets/images/img-04.jpg";
+import Image05 from "@/assets/images/img-05.jpg";
+import Image06 from "@/assets/images/img-06.jpg";
+import Image07 from "@/assets/images/img-07.jpg";
+import Image08 from "@/assets/images/img-08.jpg";
+import Image09 from "@/assets/images/img-09.jpg";
+import Image10 from "@/assets/images/img-10.jpg";
+import Image11 from "@/assets/images/img-11.jpg";
+import Image12 from "@/assets/images/img-12.jpg";
+import Image13 from "@/assets/images/img-13.jpg";
+import Image14 from "@/assets/images/img-14.jpg";
+import Image15 from "@/assets/images/img-15.jpg";
+import Image16 from "@/assets/images/img-16.jpg";
+import Image17 from "@/assets/images/img-17.jpg";
+import Image18 from "@/assets/images/img-18.jpg";
+import Image19 from "@/assets/images/img-19.jpg";
+import Image20 from "@/assets/images/img-20.jpg";
+import Image21 from "@/assets/images/img-21.jpg";
+import Image22 from "@/assets/images/img-22.jpg";
+import Image23 from "@/assets/images/img-23.jpg";
+import Image24 from "@/assets/images/img-24.jpg";
+import Image25 from "@/assets/images/img-25.jpg";
+import Image26 from "@/assets/images/img-26.jpg";
+import Image27 from "@/assets/images/img-27.jpg";
+import Image28 from "@/assets/images/img-28.jpg";
+import Image29 from "@/assets/images/img-29.jpg";
+import Image30 from "@/assets/images/img-30.jpg";
+import Image31 from "@/assets/images/img-31.jpg";
+import Image32 from "@/assets/images/img-32.jpg";
+import Image33 from "@/assets/images/img-33.jpg";
+import Image34 from "@/assets/images/img-34.jpg";
+import Image35 from "@/assets/images/img-35.jpg";
+import Image36 from "@/assets/images/img-36.jpg";
+import Image37 from "@/assets/images/img-37.jpg";
+import Image38 from "@/assets/images/img-38.jpg";
+import Image39 from "@/assets/images/img-39.jpg";
+import Image40 from "@/assets/images/img-40.jpg";
+import Image41 from "@/assets/images/img-41.jpg";
+import Image42 from "@/assets/images/img-42.jpg";
+import Image43 from "@/assets/images/img-43.jpg";
+import Image44 from "@/assets/images/img-44.jpg";
+import Image45 from "@/assets/images/img-45.jpg";
+import Image46 from "@/assets/images/img-46.jpg";
+import Image47 from "@/assets/images/img-47.jpg";
+import Image48 from "@/assets/images/img-48.jpg";
+import Image49 from "@/assets/images/img-49.jpg";
+import Image50 from "@/assets/images/img-50.jpg";
+import Image51 from "@/assets/images/img-51.jpg";
+import Image52 from "@/assets/images/img-52.jpg";
+
+type Image = {
+  key: number;
+  alt: string;
+  path: StaticImageData;
+  width: number;
+  height: number;
+};
+
+const images: Image[] = [
+  {
+    key: 1,
+    alt: "Image 01",
+    path: Image01,
+    width: 1026,
+    height: 1280,
+  },
+  {
+    key: 2,
+    alt: "Image 02",
+    path: Image02,
+    width: 640,
+    height: 640,
+  },
+  {
+    key: 3,
+    alt: "Image 03",
+    path: Image03,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 4,
+    alt: "Image 04",
+    path: Image04,
+    width: 1280,
+    height: 1230,
+  },
+  {
+    key: 5,
+    alt: "Image 05",
+    path: Image05,
+    width: 1280,
+    height: 1233,
+  },
+  {
+    key: 6,
+    alt: "Image 06",
+    path: Image06,
+    width: 800,
+    height: 1280,
+  },
+  {
+    key: 7,
+    alt: "Image 07",
+    path: Image07,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 8,
+    alt: "Image 08",
+    path: Image08,
+    width: 1042,
+    height: 1280,
+  },
+  {
+    key: 9,
+    alt: "Image 09",
+    path: Image09,
+    width: 720,
+    height: 1280,
+  },
+  {
+    key: 10,
+    alt: "Image 10",
+    path: Image10,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 11,
+    alt: "Image 11",
+    path: Image11,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 12,
+    alt: "Image 12",
+    path: Image12,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 13,
+    alt: "Image 13",
+    path: Image13,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 14,
+    alt: "Image 14",
+    path: Image14,
+    width: 1026,
+    height: 1280,
+  },
+  {
+    key: 15,
+    alt: "Image 15",
+    path: Image15,
+    width: 1031,
+    height: 1280,
+  },
+  {
+    key: 16,
+    alt: "Image 16",
+    path: Image16,
+    width: 1026,
+    height: 1280,
+  },
+  {
+    key: 17,
+    alt: "Image 17",
+    path: Image17,
+    width: 1026,
+    height: 1280,
+  },
+  {
+    key: 18,
+    alt: "Image 18",
+    path: Image18,
+    width: 1026,
+    height: 1280,
+  },
+  {
+    key: 19,
+    alt: "Image 19",
+    path: Image19,
+    width: 1021,
+    height: 1280,
+  },
+  {
+    key: 20,
+    alt: "Image 20",
+    path: Image20,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 21,
+    alt: "Image 21",
+    path: Image21,
+    width: 1195,
+    height: 1280,
+  },
+  {
+    key: 22,
+    alt: "Image 22",
+    path: Image22,
+    width: 796,
+    height: 958,
+  },
+  {
+    key: 23,
+    alt: "Image 23",
+    path: Image23,
+    width: 579,
+    height: 1280,
+  },
+  {
+    key: 24,
+    alt: "Image 24",
+    path: Image24,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 25,
+    alt: "Image 25",
+    path: Image25,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 26,
+    alt: "Image 26",
+    path: Image26,
+    width: 1280,
+    height: 960,
+  },
+  {
+    key: 27,
+    alt: "Image 27",
+    path: Image27,
+    width: 1280,
+    height: 960,
+  },
+  {
+    key: 28,
+    alt: "Image 28",
+    path: Image28,
+    width: 1280,
+    height: 960,
+  },
+  {
+    key: 29,
+    alt: "Image 29",
+    path: Image29,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 30,
+    alt: "Image 30",
+    path: Image30,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 31,
+    alt: "Image 31",
+    path: Image31,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 32,
+    alt: "Image 32",
+    path: Image32,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 33,
+    alt: "Image 33",
+    path: Image33,
+    width: 1280,
+    height: 924,
+  },
+  {
+    key: 34,
+    alt: "Image 34",
+    path: Image34,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 35,
+    alt: "Image 35",
+    path: Image35,
+    width: 1114,
+    height: 1280,
+  },
+  {
+    key: 36,
+    alt: "Image 36",
+    path: Image36,
+    width: 1280,
+    height: 754,
+  },
+  {
+    key: 37,
+    alt: "Image 37",
+    path: Image37,
+    width: 1224,
+    height: 1280,
+  },
+  {
+    key: 38,
+    alt: "Image 38",
+    path: Image38,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 39,
+    alt: "Image 39",
+    path: Image39,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 40,
+    alt: "Image 40",
+    path: Image40,
+    width: 1280,
+    height: 858,
+  },
+  {
+    key: 41,
+    alt: "Image 41",
+    path: Image41,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 42,
+    alt: "Image 42",
+    path: Image42,
+    width: 960,
+    height: 1280,
+  },
+  {
+    key: 43,
+    alt: "Image 43",
+    path: Image43,
+    width: 640,
+    height: 640,
+  },
+  {
+    key: 44,
+    alt: "Image 44",
+    path: Image44,
+    width: 948,
+    height: 1086,
+  },
+  {
+    key: 45,
+    alt: "Image 45",
+    path: Image45,
+    width: 590,
+    height: 1280,
+  },
+  {
+    key: 46,
+    alt: "Image 46",
+    path: Image46,
+    width: 590,
+    height: 1280,
+  },
+  {
+    key: 47,
+    alt: "Image 47",
+    path: Image47,
+    width: 606,
+    height: 1280,
+  },
+  {
+    key: 48,
+    alt: "Image 48",
+    path: Image48,
+    width: 590,
+    height: 1280,
+  },
+  {
+    key: 49,
+    alt: "Image 49",
+    path: Image49,
+    width: 590,
+    height: 1280,
+  },
+  {
+    key: 50,
+    alt: "Image 50",
+    path: Image50,
+    width: 796,
+    height: 958,
+  },
+  {
+    key: 51,
+    alt: "Image 51",
+    path: Image51,
+    width: 1280,
+    height: 1277,
+  },
+  {
+    key: 52,
+    alt: "Image 52",
+    path: Image52,
+    width: 1080,
+    height: 903,
+  },
+];
+
+export default function Home() {
+  return (
+    <section>
+      <div className="container">
+        <div className="grid sm:grid-cols-2 gap-x-4 gap-y-6 pt-8">
+          {images.map((img) => (
+            <article key={img.key}>
+              <Image
+                src={img.path}
+                alt={img.alt}
+                width={img.width}
+                height={img.height}
+                priority
+              />
+            </article>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
